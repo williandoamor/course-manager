d809c367c7ca0121b49313f4b9ef761aeca03494
williandoamor <willsazon@gmail.com>
2020-12-06 10:39:35 -0300

    Criacao da classe curso e do metodo de listagem de cursos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { CursoListComponet } from './cursos/curso-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursoListComponet
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cursos/curso-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Curso } from './curso';
+
+@Component({
+
+    selector: 'app-curso-list',
+    templateUrl: './curso-list.component.html'
+
+})
+
+export class CursoListComponet implements OnInit {
+
+    cursos: Curso[] = [];
+
+    ngOnInit(): void {
+
+        this.cursos = [
+
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'xps-8796',
+                duration: 120,
+                rating: 5.4,
+                releaseDate: 'November, 2, 2019'
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '',
+                price: 45.99,
+                code: 'LKL-1894',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'November, 04, 2019'
+            }
+
+        ]
+
+    }
+}
