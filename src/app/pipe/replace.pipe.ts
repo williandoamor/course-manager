d9f34ee800ff59e87a42925abbce5783f947c414
williandoamor <willsazon@gmail.com>
2020-12-08 11:09:24 -0300

    Criacao do componente nav-bar

--- src/app/pipe/replace.pipe.ts
+++ src/app/pipe/replace.pipe.ts
@@ -3,6 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
     name: 'replace'
 })
+/*Classe criada para remover caracter e substituir por outro*/
 export class ReplacePipe implements PipeTransform {
 
     transform(value: string, char: string, valueToReplace: string) {
